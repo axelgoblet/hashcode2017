90ab665d5908f8a1574e501b6506fd21e8e2d917
Axel Goblet <axel.goblet@mediaan.com>
2017-02-23 19:28:29 +0100

    first heuristics

--- HashCode2017.Solution/HashCode2017.Solution/Program.cs
+++ HashCode2017.Solution/HashCode2017.Solution/Program.cs
@@ -11,6 +11,14 @@ namespace HashCode2017.Solution
         static void Main(string[] args)
         {
             var problem = Parser.Load("../../../Input/me_at_the_zoo.in");
+
+            problem.Endpoints = problem.Endpoints.Where(e => e.ConnectedCacheServers > 0).ToList();
+            foreach (var endpoint in problem.Endpoints)
+            {
+                endpoint.ConnectedCaches = endpoint.ConnectedCaches.Where(c => c.Latency < endpoint.Latency).ToList();
+            }
+
+
         }
     }
 }
